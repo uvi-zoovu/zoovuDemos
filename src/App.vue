64fe889b4c8ef5f007b51adf60821565a2985cfe
r.farchi <r.farchi@zoovu.com>
2022-02-17 15:33:39 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.svg">
+    <br/>
+    <FilterBar @filterRequest="filter"></FilterBar>
+    <div id="carousel">{{this.$data.activeFilters}}</div>
+
+  </div>
+</template>
+
+<script>
+import FilterBar from "@/components/FilterBar";
+
+export default {
+  name: 'App',
+  components: {FilterBar},
+  data(){
+  return {activeFilters : []}
+  },
+  methods: {
+    filter(params) {
+      if(params.status) {
+        this.$data.activeFilters.push(params.demoType)
+      }
+      else{
+        this.$data.activeFilters.splice(this.$data.activeFilters.indexOf(params.demoType),1)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FilterBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <form class='filterBar'>
+      <h1 class='filterHeader'>Demo Type</h1>
+      <fieldset v-for='demoType in demoTypes' v-bind:key='demoType'>
+        <input
+            type='checkbox'
+            :id=demoType
+            :value=demoType
+            @change='$emit("filterRequest", {demoType: demoType, status: $event.target.checked})'
+        >
+        <input
+            v-if='index != demoTypes.length - 1'
+            type='button'
+            id=demoTypesAll
+            @click='$emit("filterRequest", {demoType: demoType, status: $event.target.checked})'
+        >
+        <label :for=demoType>{{ demoType.toUpperCase() }}</label>
+      </fieldset>
+
+
+        <h1 class='filterHeader'>Demo Industry</h1>
+        <fieldset v-for='demoType in demoTypes' v-bind:key='demoType'>
+          <input
+              type='checkbox'
+              :id=demoType
+              :value=demoType
+              @change='$emit("filterRequest", {demoType: demoType, status: $event.target.checked})'
+          >
+          <label :for=demoType>{{ demoType.toUpperCase() }}</label>
+        </fieldset>
+
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterBar',
+  emits:['filterRequest'],
+  data() {
+    return {
+      demoTypes: ['assistant', 'configurator', 'others'],
+      demoIndustries: []
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.filterBar{
+  width: 10vw;
+  background: aliceblue;
+}
+</style>
